8acff68c5145b6acf30393aae03af87da73c67c2
ec2ainun <ec2ainun@gmail.com>
2019-09-17 09:05:31 +0800

    fix nginx

--- registry/configNginx.sh
+++ registry/configNginx.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install nginx
+sudo apt update -y
+sudo apt install nginx -y
 sudo ufw allow 'Nginx Full'
-sudo ufw enable
 
 sudo mkdir -p /var/www/registry.algo.fit/html
 sudo chown -R $USER:$USER /var/www/registry.algo.fit/html
 sudo chmod -R 755 /var/www/registry.algo.fit
 
 cp -r registry.algo.fit/ /var/www/
-cp -r sites-available/ etc/nginx/
+cp -r sites-available/ /etc/nginx/
 
 sudo ln -s /etc/nginx/sites-available/registry.algo.fit /etc/nginx/sites-enabled/
 
