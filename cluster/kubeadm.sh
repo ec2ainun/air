a78e35f0b42217054bde87af1ac3323772a7fb54
ec2ainun <ec2ainun@gmail.com>
2019-09-23 15:41:28 +0800

    fix kubeadm

--- cluster/kubeadm.sh
+++ cluster/kubeadm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 wget https://gist.githubusercontent.com/ec2ainun/6ed0d54afe2d76bad2e038dbb3fb21b1/raw/91852c38301a4e509de68cb57237e2362bba66e8/docker18.sh
+bash docker18.sh
 sudo swapoff -a
 
 apt-get update && apt-get install -y apt-transport-https curl
