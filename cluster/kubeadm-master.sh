0ea1ce971118875c3b3fef03219bca31ce2e1de4
ec2ainun <ec2ainun@gmail.com>
2019-09-23 16:50:14 +0800

    test script kubeadm

--- cluster/kubeadm-master.sh
+++ cluster/kubeadm-master.sh
@@ -2,5 +2,7 @@
 bash kube.sh
 kubeadm init --kubernetes-version $(kubeadm version -o short)
 
-export KUBECONFIG=/etc/kubernetes/admin.conf
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
