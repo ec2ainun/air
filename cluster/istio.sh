3c94a4aa23b3cfd359ad3f99223cdc24cbb1e08d
ec2ainun <ec2ainun@gmail.com>
2019-09-24 11:54:02 +0800

    istio set path

--- cluster/istio.sh
+++ cluster/istio.sh
@@ -2,7 +2,4 @@
 curl -L https://git.io/getLatestIstio | ISTIO_VERSION=1.3.0 sh -
 cd istio-1.3.0
 
-if [[ ! -d "$ISTIOCTL" ]]; then 
-    export PATH=$PWD/bin:$PATH
-    echo "export PATH=\"$(pwd)/bin\"" >> ~/.bashrc
-fi
+echo "export PATH=\"$(pwd)/bin\"" >> ~/.bashrc
